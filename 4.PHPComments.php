2a620d375570691145438497445b90b84298060f
Hendryck Permana <hendryck.permana@gmail.com>
2025-04-24 22:48:29 +0800

    Create 4.PHPComments.php

---
+++ 4.PHPComments.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Outputs a welcome message:
+# Outputs a welcome message:
+/*Output a
+welcome message*/
+
+echo "Welcome Home!";
+?>
+ 
+</body>
+</html>
